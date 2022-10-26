425997435f9245d6f00d1a79a04cb5e26ac5887e
Kasan <879611700@qq.com>
2022-10-26 12:55:27 +0800

    props的用法和组件通信

--- src/C1.vue
+++ src/C1.vue
@@ -1,11 +1,7 @@
 <template>
     <div class="p1">
-        <button @click="$emit('update:xxx',xxx+100)">可以运算+100</button>
-        <button @click="$emit('update:xxx',`这是字符串`)">可以直接给结果，字符串</button>
-        <button @click="$emit('update:xxx',10000)">可以直接给结果，数字</button>
-        <button @click="$emit('update:xxx',xxx=10086)">会报警告，不能直接赋值</button>
-        <!-- 也可以像下面这样写 -->
-        <!-- <button @click="x1">button2</button> -->  
+        <button @click="add">可以运算+100</button>
+        <button @click="change">可以直接给结果</button>
     </div>
 </template>
     
@@ -14,13 +10,7 @@
 <script>
     
 export default {
-        props: ["xxx"],
-        methods: {
-          x1(){
-            this.$emit('update:xxx',299)
-            console.log('值已改变')
-          }
-        },
+        props: ["xxx","add","change"],
     };
     
     
