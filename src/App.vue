83d679b9542c694307c526b3128b6a7b62103552
Kasan <879611700@qq.com>
2022-10-27 06:53:23 +0800

    v-model 和 双向绑定

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,34 @@
 <template>
   <div class="app">
-      这是xxx的值：{{xxx}}
-      <C1/>
+      用户信息：{{user}}  
+     
+      <!-- C1是C1.vue里的输入框，但是要改App.vue的数据，这里有两个输入框，按这种方式
+      是不能用v-model的，因为$event不能区分哪个是用户名哪个是密码，会造成
+      数据污染，只能通过这种非语法糖的方式拿到event的具体属性 -->
+      <C1  :value="user.userName"  
+      :value1="user.userMia" 
+      @input="user.userName = $event.mingZi, user.userMia = $event.miMa" />
+
+
+   APP组件的： <input type="text" :value="user.userName" @input="user.userName=$event.target.value"/>
+   APP组件的： <input type="text" v-model="user.userMia" />
   </div>
 </template>
 
 
 
 <script>
-import C1 from "./C1.vue"
+ import C1 from "./C1.vue"
 export default {
   data() {
-    return {xxx:100,};
-  },
-  components: {C1},
-  provide(){
     return {
-      xxx:this.xxx,
-      add:this.add,
-      change:this.change,
-    }
-  },
-  methods: {
-    add(){
-        this.xxx += 100
-        console.log(this.xxx)
-      },
-      change(){
-        this.xxx = 2223333
-        console.log(this.xxx)
+      user:{
+        userName:"1",
+        userMia:'2'
       }
+    };
   },
+   components: {C1},
 };
 </script>
 
--- src/C1.vue
+++ src/C1.vue
@@ -1,25 +1,42 @@
 <template>
-    <div class="p1">
-         <button @click="add">可以运算+100</button>
-        <button @click="change">可以直接给结果</button> 
-    </div>
-</template>
-    
+  <div class="p1">
+  
+    C1组件的 用户名：<input  :value="value"   @input="oninput" />
+    C1组件的 密码：  <input  :value="value1"  @input='oninput1'/> 
+          
+  </div>
 
+</template>
+  
 
 <script>
-    
 export default {
-        inject:["xxx","add","change"],
-    };
-    
-    
+        props: ["value","value1"],
+       data(){
+        return {
+        user:{
+            mingZi: '占位',
+            miMa: '占位',
+          }
+        }
+       },
+     methods:{
+      oninput(e){
+        this.user.mingZi = e.target.value
+          this.$emit('input',this.user)
+      },
+      oninput1(e){
+        this.user.miMa = e.target.value
+          this.$emit('input',this.user)
+      },
+     }
+  };
 </script>
-    
-    
+  
+  
 <style>
-  .p1{
-    padding: 10px;
-  }
-    
+.p1{
+  padding: 10px;
+}
+  
 </style>
